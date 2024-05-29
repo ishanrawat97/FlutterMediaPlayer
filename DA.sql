679bda15e2aa131ca46db6bf3d03bfeb128132ea
ishanrawat97 <ishanrawat97>
2024-05-29 18:54:46 +0530

    newnew Commit

--- DA.sql
+++ DA.sql
@@ -1,25 +1,19 @@
-/*
- * ER/Studio Data Architect SQL Code Generation
- * Project :      demo1.DM1
- *
- * Date Created : Tuesday, April 30, 2024 17:17:20
- * Target DBMS : Databricks
- */
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model1git.DM1
+--
+-- Date Created : Wednesday, May 29, 2024 18:54:42
+-- Target DBMS : PostgreSQL 10.x-12.x
+--
 
-/* 
- * TABLE: prema.Entity1 
- */
+-- 
+-- TABLE: "Entity1" 
+--
 
-CREATE TABLE prema.Entity1
-(
-    test1    string,
-    test2    timestamp,
-    test3    tinyint,
-    test4    bigint            GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1)  NOT NULL,
-    test5    smallint,
-    test6    decimal(10, 0),
-    test7    boolean,
-    test8    timestamp_ntz
+CREATE TABLE "Entity1"(
+
 )
 ;
+
+
 
