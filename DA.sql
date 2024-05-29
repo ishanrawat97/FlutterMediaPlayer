dff5f6f564bd3efb01f303d65447cc39bdb7106c
ishanrawat97 <ishanrawat97>
2024-05-29 19:41:43 +0530

    some new message

--- DA.sql
+++ DA.sql
@@ -2,7 +2,7 @@
 -- ER/Studio Data Architect SQL Code Generation
 -- Project :      Model1git.DM1
 --
--- Date Created : Wednesday, May 29, 2024 19:28:47
+-- Date Created : Wednesday, May 29, 2024 19:41:43
 -- Target DBMS : PostgreSQL 10.x-12.x
 --
 
@@ -11,7 +11,7 @@
 --
 
 CREATE TABLE "Entity1"(
-
+    new_col    character(10)
 )
 ;
 
