217f8fbf2f6f3d78e49703569e528a7414153cf6
ishanrawat97 <ishanrawat97>
2024-05-30 06:01:58 +0530

    new message

---
+++ dtbFE.sql
@@ -0,0 +1,19 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model1git.DM1
+--
+-- Date Created : Thursday, May 30, 2024 06:01:58
+-- Target DBMS : PostgreSQL 10.x-12.x
+--
+
+-- 
+-- TABLE: "Entity1" 
+--
+
+CREATE TABLE "Entity1"(
+
+)
+;
+
+
+
