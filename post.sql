369277e059d62c5485ddb4cc34f5db4ef3f5b90d
ishanrawat97 <ishanrawat97>
2024-05-30 08:40:58 +0530

    bang

---
+++ post.sql
@@ -0,0 +1,19 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model1git.DM1
+--
+-- Date Created : Thursday, May 30, 2024 08:40:57
+-- Target DBMS : PostgreSQL 10.x-12.x
+--
+
+-- 
+-- TABLE: "Entity1" 
+--
+
+CREATE TABLE "Entity1"(
+
+)
+;
+
+
+
