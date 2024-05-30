5745d4824bc71f1173aac86328770d1e6b6d3e9d
ishanrawat97 <ishanrawat97>
2024-05-30 13:04:39 +0530

    new commit

---
+++ n.sql
@@ -0,0 +1,20 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      DATA MODEL
+--
+-- Date Created : Thursday, May 30, 2024 13:04:39
+-- Target DBMS : Snowflake
+--
+
+-- 
+-- TABLE: Entity1 
+--
+
+CREATE TABLE Entity1(
+    f    CHAR(10),
+    g    CHAR(10)
+)
+;
+
+
+
