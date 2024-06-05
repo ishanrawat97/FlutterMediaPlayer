f0c75a136b69934c9f5fd82873f04bb16767fbb7
ishanrawat97 <ishanrawat97>
2024-06-05 18:22:57 +0530

    hgg

--- DA.sql
+++ DA.sql
@@ -1,6 +1,19 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model1git.DM1
+--
+-- Date Created : Wednesday, June 05, 2024 18:22:57
+-- Target DBMS : Snowflake
+--
 
--- Standard Alter Table SQL
+-- 
+-- TABLE: Entity1 
+--
 
-ALTER TABLE "Entity1"
-    RENAME COLUMN cc TO z
+CREATE TABLE Entity1(
+    z    CHAR(10)
+)
 ;
+
+
+
