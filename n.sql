e7fb3f3aed9f34520a7214d322886ccb25c7b277
ishanrawat97 <ishanrawat97>
2024-06-10 17:08:06 +0530

    newnewen

--- n.sql
+++ n.sql
@@ -2,7 +2,7 @@
 -- ER/Studio Data Architect SQL Code Generation
 -- Project :      Model1git.DM1
 --
--- Date Created : Monday, June 10, 2024 17:05:54
+-- Date Created : Monday, June 10, 2024 17:07:38
 -- Target DBMS : Snowflake
 --
 
