2def8d2090630a2eecb404a6e37c2f963cc41819
ishanrawat97 <ishanrawat97>
2024-06-12 19:42:03 +0530

    asdwqr

--- newnew/n_alter.sql
+++ newnew/n_alter.sql
@@ -1,22 +1,6 @@
---
--- ER/Studio Data Architect SQL Code Generation
--- Project :      Model1git.DM1
---
--- Date Created : Wednesday, June 12, 2024 19:06:20
--- Target DBMS : Snowflake
---
 
--- 
--- TABLE: Entity1 
---
+-- Standard Alter Table SQL
 
-CREATE TABLE Entity1(
-    z     CHAR(10),
-    a     CHAR(10),
-    b     CHAR(10),
-    cd    CHAR(10)
-)
+ALTER TABLE "Entity1"
+    RENAME COLUMN cd TO cde
 ;
-
-
-
