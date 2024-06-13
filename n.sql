2e8573b1e8dab663e41fc08d843e2d08ea504670
ishanrawat97 <ishanrawat97>
2024-06-13 22:11:54 +0530

    a

--- n.sql
+++ n.sql
@@ -1,3 +1,14 @@
-123
-4
-3
+
+-- Standard Alter Table SQL
+
+ALTER TABLE "Entity1"
+    ADD COLUMN a char(10)
+;
+
+ALTER TABLE "Entity1"
+    ADD COLUMN b char(10)
+;
+
+ALTER TABLE "Entity1"
+    ADD COLUMN cd char(10)
+;
