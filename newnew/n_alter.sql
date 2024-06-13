8c6ca9862f721211ff95c554fa2ebad74e67c0fe
ishanrawat97 <ishanrawat97>
2024-06-13 21:00:03 +0530

    2134

--- newnew/n_alter.sql
+++ newnew/n_alter.sql
@@ -1,22 +1,12 @@
---
--- ER/Studio Data Architect SQL Code Generation
--- Project :      Model1git.DM1
---
--- Date Created : Thursday, June 13, 2024 16:07:42
--- Target DBMS : Snowflake
---
 
--- 
--- TABLE: Entity1 
---
+-- Drop Constraint, Rename and Create Table SQL
 
-CREATE TABLE Entity1(
-    z        CHAR(10),
-    a        CHAR(10),
-    aasdf    CHAR(10),
-    cd       CHAR(10)124125
+CREATE TABLE "Entity1"
+(
+    z  char(10),
+    a  char(10),
+    b  char(10),
+    cd char(10)
 )
+
 ;
-
-
-
