e6a518f6be49b6e91b35ab2dc91751bafc1fb6e5
ishanrawat97 <ishanrawat97>
2024-06-14 13:24:46 +0530

    safsa

---
+++ newnew/n_alter.sql
@@ -0,0 +1,22 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model1git.DM1
+--
+-- Date Created : Friday, June 14, 2024 13:24:45
+-- Target DBMS : Snowflake
+--
+
+-- 
+-- TABLE: Entity1 
+--
+
+CREATE TABLE Entity1(
+    z     CHAR(10),
+    a     CHAR(10),
+    b     CHAR(10),
+    cd    CHAR(10)
+)
+;
+
+
+
