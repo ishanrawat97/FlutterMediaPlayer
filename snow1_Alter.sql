10b2ac2a06390b6de5a8f98e0cd5ee51a1e723cf
ishanrawat97 <ishanrawat97>
2024-06-14 16:21:22 +0530

    new alter

---
+++ snow1_Alter.sql
@@ -0,0 +1,18 @@
+
+-- Standard Alter Table SQL
+
+ALTER TABLE "Entity1"
+    RENAME COLUMN z TO asad
+;
+
+ALTER TABLE "Entity1"
+    ADD COLUMN a char(10)
+;
+
+ALTER TABLE "Entity1"
+    ADD COLUMN b char(10)
+;
+
+ALTER TABLE "Entity1"
+    ADD COLUMN cd char(10)
+;
