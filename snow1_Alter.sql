7d4e7050607ac05252823ef0bd271f66f21d4979
ishanrawat97 <ishanrawat97>
2024-06-14 16:23:14 +0530

    new alter selective

--- snow1_Alter.sql
+++ snow1_Alter.sql
@@ -2,17 +2,5 @@
 -- Standard Alter Table SQL
 
 ALTER TABLE "Entity1"
-    RENAME COLUMN z TO asad
-;
-1234
-ALTER TABLE "Entity1"
-    ADD COLUMN a char(10)
-;
-
-ALTER TABLE "Entity1"
-    ADD COLUMN b char(10)
-;
-
-ALTER TABLE "Entity1"
-    ADD COLUMN cd char(10)
+    ADD COLUMN ab char(10)
 ;
