50a9dbb4ff48902a9d7e1f8824c645c4c4be0397
ishanrawat97 <ishanrawat97>
2024-06-17 22:35:18 +0530

    567

--- newnew/n_alter.sql
+++ newnew/n_alter.sql
@@ -2,7 +2,7 @@
 -- ER/Studio Data Architect SQL Code Generation
 -- Project :      Model1git.DM1
 --
--- Date Created : Monday, June 17, 2024 22:33:31
+-- Date Created : Monday, June 17, 2024 22:35:17
 -- Target DBMS : Snowflake
 --
 
