42d8987ad9538b09c7b4eb691cbd79178c6ece95
ishanrawat97 <ishanrawat97>
2024-06-21 18:35:01 +0530

    k

---
+++ n.sql
@@ -0,0 +1,22 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model1git3.DM1
+--
+-- Date Created : Friday, June 21, 2024 18:35:01
+-- Target DBMS : Snowflake
+--
+
+-- 
+-- TABLE: Entity1 
+--
+
+CREATE TABLE Entity1(
+    z     CHAR(10),
+    a     CHAR(10),
+    b     CHAR(10),
+    cd    CHAR(10)
+)
+;
+
+
+
